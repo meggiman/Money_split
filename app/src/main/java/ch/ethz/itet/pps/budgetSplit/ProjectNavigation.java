e55f9a69e41c62c854032946a158b539d4d84529
Manuel Eggimann <meggimann@student.ethz.ch>
2014-10-08 10:23:07 +0200

    Created a Navigation tab and started to implement to ProjectOverview fragment.

---
+++ app/src/main/java/ch/ethz/itet/pps/budgetSplit/ProjectNavigation.java
@@ -0,0 +1,176 @@
+package ch.ethz.itet.pps.budgetSplit;
+
+import java.util.Locale;
+
+import android.app.Activity;
+import android.app.ActionBar;
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
+import android.net.Uri;
+import android.support.v13.app.FragmentPagerAdapter;
+import android.os.Bundle;
+import android.support.v4.view.ViewPager;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class ProjectNavigation extends Activity implements ActionBar.TabListener {
+
+    /**
+     * The {@link android.support.v4.view.PagerAdapter} that will provide
+     * fragments for each of the sections. We use a
+     * {@link FragmentPagerAdapter} derivative, which will keep every
+     * loaded fragment in memory. If this becomes too memory intensive, it
+     * may be best to switch to a
+     * {@link android.support.v13.app.FragmentStatePagerAdapter}.
+     */
+    SectionsPagerAdapter mSectionsPagerAdapter;
+
+    /**
+     * The {@link ViewPager} that will host the section contents.
+     */
+    ViewPager mViewPager;
+
+    /**
+     * Uri of the Project that is loaded at the moment.
+     */
+    Uri projectContentUri;
+
+    /**
+     * Extra messages to use for Intent to start this Activity.
+     */
+    public final static String EXTRA_CONTENT_URI = "projectContentUri";
+    public final static String EXTRA_PROJECT_TITLE = "projectTitle";
+
+    /**
+     * Integer constants to identify the sections of our SectionAdapter
+     */
+    private final static int SECTION_OVERVIEW = 0;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_project_navigation);
+
+        // Set up the action bar.
+        final ActionBar actionBar = getActionBar();
+        actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
+
+        // Create the adapter that will return a fragment for each of the three
+        // primary sections of the activity.
+        mSectionsPagerAdapter = new SectionsPagerAdapter(getFragmentManager());
+
+        // Set up the ViewPager with the sections adapter.
+        mViewPager = (ViewPager) findViewById(R.id.pager);
+        mViewPager.setAdapter(mSectionsPagerAdapter);
+
+        // When swiping between different sections, select the corresponding
+        // tab. We can also use ActionBar.Tab#select() to do this if we have
+        // a reference to the Tab.
+        mViewPager.setOnPageChangeListener(new ViewPager.SimpleOnPageChangeListener() {
+            @Override
+            public void onPageSelected(int position) {
+                actionBar.setSelectedNavigationItem(position);
+            }
+        });
+
+        // For each of the sections in the app, add a tab to the action bar.
+        for (int i = 0; i < mSectionsPagerAdapter.getCount(); i++) {
+            // Create a tab with text corresponding to the page title defined by
+            // the adapter. Also specify this Activity object, which implements
+            // the TabListener interface, as the callback (listener) for when
+            // this tab is selected.
+            actionBar.addTab(
+                    actionBar.newTab()
+                            .setText(mSectionsPagerAdapter.getPageTitle(i))
+                            .setTabListener(this));
+        }
+
+        //Set Activity title that appear in the upper left corner of the screen to match the project name.
+        setTitle(getIntent().getStringExtra(EXTRA_PROJECT_TITLE));
+        //Get the Uri of the project to be load out of the Intent bundle.
+        projectContentUri = getIntent().getParcelableExtra(EXTRA_CONTENT_URI);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.project_navigation, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onTabSelected(ActionBar.Tab tab, FragmentTransaction fragmentTransaction) {
+        // When the given tab is selected, switch to the corresponding page in
+        // the ViewPager.
+        mViewPager.setCurrentItem(tab.getPosition());
+    }
+
+    @Override
+    public void onTabUnselected(ActionBar.Tab tab, FragmentTransaction fragmentTransaction) {
+    }
+
+    @Override
+    public void onTabReselected(ActionBar.Tab tab, FragmentTransaction fragmentTransaction) {
+    }
+
+    /**
+     * A {@link FragmentPagerAdapter} that returns a fragment corresponding to
+     * one of the sections/tabs/pages.
+     */
+    public class SectionsPagerAdapter extends FragmentPagerAdapter {
+
+        public SectionsPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            // getItem is called to instantiate the fragment for the given page.
+            // Return the correct Fragment for each tab.
+            switch (position) {
+                case SECTION_OVERVIEW:
+                    return ProjectOverview.newInstance(projectContentUri);
+                default:
+                    throw new IllegalArgumentException("Illegal Section Number: " + position);
+            }
+        }
+
+        @Override
+        public int getCount() {
+            // Show 3 total pages.
+            return 3;
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            Locale l = Locale.getDefault();
+            switch (position) {
+                case SECTION_OVERVIEW:
+                    return getString(R.string.title_section_overview).toUpperCase(l);
+                case 1:
+                    return getString(R.string.title_section2).toUpperCase(l);
+                case 2:
+                    return getString(R.string.title_section3).toUpperCase(l);
+            }
+            return null;
+        }
+    }
+}
---
+++ app/src/main/java/ch/ethz/itet/pps/budgetSplit/ProjectOverview.java
@@ -0,0 +1,62 @@
+package ch.ethz.itet.pps.budgetSplit;
+
+
+import android.net.Uri;
+import android.os.Bundle;
+import android.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link ProjectOverview#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class ProjectOverview extends Fragment {
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String PROJECT_CONTENT_URI = "projectContentUri";
+
+    // TODO: Rename and change types of parameters
+    private Uri projectUri;
+
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param contentUri Parameter 1.
+     * @return A new instance of fragment ProjectOverview.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static ProjectOverview newInstance(Uri contentUri) {
+        ProjectOverview fragment = new ProjectOverview();
+        Bundle args = new Bundle();
+        args.putParcelable(PROJECT_CONTENT_URI, contentUri);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    public ProjectOverview() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            projectUri = getArguments().getParcelable(PROJECT_CONTENT_URI);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_project_overview, container, false);
+    }
+
+
+}
