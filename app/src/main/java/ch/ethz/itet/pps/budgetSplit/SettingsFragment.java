4c18b4128f2b62f09a08121a75cc4e06b64b049e
Manuel Eggimann <meggimann@student.ethz.ch>
2014-10-09 21:55:02 +0200

    Added simple preferences Screen. Not linked yet.

---
+++ app/src/main/java/ch/ethz/itet/pps/budgetSplit/SettingsFragment.java
@@ -0,0 +1,29 @@
+package ch.ethz.itet.pps.budgetSplit;
+
+
+import android.os.Bundle;
+import android.app.Fragment;
+import android.preference.PreferenceFragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class SettingsFragment extends PreferenceFragment {
+
+
+    public SettingsFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        addPreferencesFromResource(R.xml.preferences);
+    }
+}
