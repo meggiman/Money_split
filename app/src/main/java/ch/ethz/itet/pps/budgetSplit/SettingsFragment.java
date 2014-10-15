3ef260e475e832e835c5c9a1a482e00e86a1bfd2
Manuel Eggimann <meggimann@student.ethz.ch>
2014-10-16 00:38:57 +0200

    Added default currency to settings activity.

--- app/src/main/java/ch/ethz/itet/pps/budgetSplit/SettingsFragment.java
+++ app/src/main/java/ch/ethz/itet/pps/budgetSplit/SettingsFragment.java
@@ -1,13 +1,19 @@
 package ch.ethz.itet.pps.budgetSplit;
 
 
+import android.database.Cursor;
 import android.os.Bundle;
 import android.app.Fragment;
+import android.preference.ListPreference;
 import android.preference.PreferenceFragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+
+import java.util.ArrayList;
+
+import ch.ethz.itet.pps.budgetSplit.contentProvider.budgetSplitContract;
 
 
 /**
@@ -25,5 +31,22 @@ public class SettingsFragment extends PreferenceFragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.preferences);
+
+        //Populate ListPreference defaultCurrency with data from database.
+        String[] projection = {budgetSplitContract.currencies.COLUMN_NAME, budgetSplitContract.currencies.COLUMN_CURRENCY_CODE};
+        Cursor cursor = getActivity().getContentResolver().query(budgetSplitContract.currencies.CONTENT_URI, projection, null, null, budgetSplitContract.currencies.COLUMN_NAME);
+        if (cursor.getColumnCount() > 0) {
+            String[] currencies = new String[cursor.getColumnCount()];
+            String[] currencyCodes = new String[cursor.getColumnCount()];
+            for (cursor.moveToFirst(); !cursor.isAfterLast(); cursor.moveToNext()) {
+                currencies[cursor.getPosition()] = cursor.getString(0);
+                currencyCodes[cursor.getPosition()] = cursor.getString(1);
+            }
+            ListPreference defaultCurrency = (ListPreference) findPreference(getResources().getString(R.string.pref_default_currency));
+            defaultCurrency.setEntries(currencies);
+            defaultCurrency.setEntryValues(currencyCodes);
+
+
+        }
     }
 }
